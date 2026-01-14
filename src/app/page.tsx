fdc15beb60d91609893f9de9515634c8abec88c9
Chronique <adhi.chronique@gmail.com>
2026-01-14 22:10:16 +0700

    fix metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,8 @@ export async function generateMetadata(): Promise<Metadata> {
       "fc:frame:image": METADATA.bannerImageUrl,
       "fc:frame:cast_action:canonical_domain": CANONICAL_DOMAIN,
       
-     
+     "base:app_id": "6966c5b3cf19b2d92b9f7348",
+
     },
   };
 }
