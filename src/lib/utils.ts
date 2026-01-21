465d2b941eb87f3c3921c6544b8c0f4d327ff5bc
Chronique <adhi.chronique@gmail.com>
2026-01-22 03:30:13 +0700

    fix url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export const METADATA = {
   
   bannerImageUrl: 'https://base-royal-servant.vercel.app/banner.png',
   iconImageUrl: 'https://base-royal-servant.vercel.app/icon.png',
-  homeUrl: process.env.NEXT_PUBLIC_URL ?? 'https://base-royal-servant.vercel.app/icon.png',
+  homeUrl: process.env.NEXT_PUBLIC_URL ?? 'https://base-royal-servant.vercel.app',
   splashBackgroundColor: "#FFFFFF"
   
 }
